e734f4bb857c10bbc05267b231160c325b960bc7
NoSequel <nosequel@protonmail.com>
2021-11-21 09:39:51 +0100

    Field#isAccessible checks

--- shared/src/main/java/io/github/nosequel/config/Configuration.java
+++ shared/src/main/java/io/github/nosequel/config/Configuration.java
@@ -38,7 +38,7 @@ public abstract class Configuration {
         file.load();
 
         for (Field field : this.getClass().getDeclaredFields()) {
-            if (!field.isAnnotationPresent(Configurable.class)) {
+            if (!field.isAccessible() && !field.isAnnotationPresent(Configurable.class)) {
                 continue;
             }
 
@@ -71,9 +71,7 @@ public abstract class Configuration {
 
     public void save() throws IllegalAccessException {
         for (Field field : this.getClass().getDeclaredFields()) {
-            field.setAccessible(true);
-
-            if (!field.isAnnotationPresent(Configurable.class)) {
+            if (!field.isAccessible() && !field.isAnnotationPresent(Configurable.class)) {
                 continue;
             }
 
